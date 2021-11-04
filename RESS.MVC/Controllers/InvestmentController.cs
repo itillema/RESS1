77960e252ad7c0e61572f651589ef847edb1f84b
Isaac Tillema <itillema@ricssoftware.com>
2021-11-04 16:34:25 -0400

    Added Investment and Management Views and COntrollers

---
+++ RESS.MVC/Controllers/InvestmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RESS.MVC.Controllers
+{
+    public class InvestmentController : Controller
+    {
+        // GET: Investment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
